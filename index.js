f2a9aa29ab637f90731b76b4dba044b107a38a86
Green-Elephant <dflukava@gmail.com>
2018-10-29 02:54:48 +0300

    first commit

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ async function checkAllURLs() {
     const browser = await puppeteer.launch(browserOptions);
     const page = await browser.newPage();
     await page.setCookie({ "name": "AIOHTTP_SESSION", "value": "2772e16f0f42479bb1a2cb3dec43f9c7", "domain": "reg.nti-contest.ru", "path": "/", "expires": -1, "size": 19, "httpOnly": false, "secure": false, "session": false })
-    for (let i = len; i >= 0; i--) {
+    for (let i = len-1; i >= 0; i--) {
         console.log(`id: ${len-i}`)
         console.log('ids[i]: ', ids[i])
         //await delay(1000);
